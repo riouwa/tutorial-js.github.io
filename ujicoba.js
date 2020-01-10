162624828b7f17f0a02323ebd4e77bc3616fe6c4
Dwi Satria Utama <riou.satria@gmail.com>
2020-01-10 17:30:10 +0700

    Add files via upload

---
+++ ujicoba.js
@@ -0,0 +1,7 @@
+function tombolUbah() {
+  document.getElementById("demo").innerHTML = "Ubah paragraf";
+}
+function helloWorld() {
+  document.getElementById("demo").innerHTML = "Hello world";
+  document.getElementById("myImage").src = "thumb_IMG_8153_1024.jpg";
+}
